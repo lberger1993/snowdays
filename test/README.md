c015c56fb10ee5c7f8aebc2d46ff496d58b4f9cb
lberger1993 <berger.luciaeve@gmail.com>
2017-11-23 16:49:07 +0100

    simple example to getting chrome running

---
+++ test/README.md
@@ -0,0 +1,8 @@
+# To Run Tests:
+
+`npm install selenium-webdriver`
+
+# Install web-driver:
+`brew install chromedriver`
+
+# Install the Chrome Driver in correct path
---
+++ test/example.js
@@ -0,0 +1,10 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .build();
+
+driver.get('http://www.google.com/);
+// driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+// driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.quit();
