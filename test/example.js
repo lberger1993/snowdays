3da976f1eb16546a1733926bb2f0aa86e51174d3
lberger1993 <berger.luciaeve@gmail.com>
2017-11-23 16:50:04 +0100

    to fix the selenium

--- test/example.js
+++ test/example.js
@@ -4,7 +4,7 @@ let driver = new Builder()
     .forBrowser('chrome')
     .build();
 
-driver.get('http://www.google.com/);
+driver.get('http://www.google.com/');
 // driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
 // driver.wait(until.titleIs('webdriver - Google Search'), 1000);
 driver.quit();
